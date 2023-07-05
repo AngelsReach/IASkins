24f847c0caee1f7a681f74b8a88bc7833fc7e76e
Dustin Mundy <VergilPrime@Angels-Reach.com>
2023-07-04 19:51:07 -0500

    Added PlayerController.restoreSkins, comments to explain every single method in the controllers.

--- src/main/java/com/vergilprime/iaskins/IASkins.java
+++ src/main/java/com/vergilprime/iaskins/IASkins.java
@@ -3,15 +3,10 @@ package com.vergilprime.iaskins;
 import com.vergilprime.iaskins.controllers.PlayerController;
 import com.vergilprime.iaskins.controllers.SkinsController;
 import dev.lone.itemsadder.api.Events.ItemsAdderLoadDataEvent;
-import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.EventHandler;
 import org.mineacademy.fo.plugin.SimplePlugin;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
 
 
 public final class IASkins extends SimplePlugin {
@@ -23,30 +18,13 @@ public final class IASkins extends SimplePlugin {
 
 	@Override
 	public void onPluginStart() {
+		// Do we need to register the onItemsAdderLoadDataEvent or something?
 	}
 
 	@EventHandler
 	public void onItemsAdderLoadDataEvent(ItemsAdderLoadDataEvent event) {
-		//skins = skinsConfig.get < Map < String, Map < String, String >>> ("skins");
-		try {
-			skinsConfig.load("skins.yml");
-		} catch (Exception e) {
-			this.getLogger().log(Level.SEVERE, "Some file bullshit", e);
-		}
-		//skins.yml the file that holds the info we need.
-		for (String skinName : skinsConfig.getKeys(false)) {
-			ConfigurationSection section = skinsConfig.getConfigurationSection(skinName);
-			if (section == null) {
-				continue;
-			}
-			Map<String, String> skinPairs = new HashMap<>();
-			for (String itemType : section.getKeys(false)) {
-				skinPairs.put(itemType, section.getString(itemType));
-				skinsController.skinsReversed.put(section.getString(itemType), skinName);
-			}
-			skinsController.skins.put(skinName, skinPairs);
-		}
-
+		playerController = new PlayerController(this);
+		skinsController = new SkinsController(this);
 	}
 
 }
